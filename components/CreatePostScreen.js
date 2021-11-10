77d4fe58d4fe42df06410b38229e7b89e3f01716
faizansahib <faizan.akash99@gmail.com>
2021-11-10 11:46:23 +0000

    first commit

---
+++ components/CreatePostScreen.js
@@ -0,0 +1,44 @@
+
+
+import * as React from 'react';
+import { Text, TextInput,  Button, StyleSheet } from 'react-native';
+
+
+
+export default  function CreatePostScreen({ navigation, route }) {
+    const [postText, setPostText] = React.useState('');
+  
+    return (
+      <>
+        <TextInput
+          multiline
+          placeholder="What's on your mind?"
+          style={{ height: 200, padding: 10, backgroundColor: 'white' }}
+          value={postText}
+          onChangeText={setPostText}
+        />
+        <Button
+          title="Done"
+          onPress={() => {
+            // Pass and merge params back to home screen
+            navigation.navigate({
+              name: 'Home',
+              params: { post: postText },
+              merge: true,
+            });
+          }}
+        />
+      </>
+    );
+
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  
